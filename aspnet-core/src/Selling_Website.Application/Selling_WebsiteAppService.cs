e75671f7cc100950c92caddd32214264e4c34420
Vinh Hoai <kevinlam812@gmail.com>
2020-11-08 16:46:26 +0700

    update name & info

--- aspnet-core/src/Selling_Website.Application/Selling_WebsiteAppService.cs
+++ aspnet-core/src/Selling_Website.Application/Selling_WebsiteAppService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Selling_Website.Localization;
+﻿using Selling_Website.Localization;
 using Volo.Abp.Application.Services;
 
 namespace Selling_Website
